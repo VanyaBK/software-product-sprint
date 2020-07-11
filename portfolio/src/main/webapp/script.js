74b54047ce64e206abb9a06b3743f9865a09e6a8
VanyaBK <vanyabk999@gmail.com>
2020-07-12 00:29:21 +0530

    First Commit

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['How you doin?', 'Joey doesnt share food!', 'I know!!!!', 'We were on a breeaaak'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
